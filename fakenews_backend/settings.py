c3aa823dd993e4e506fa9c715c82cc2a72e25f90
chandlerlei2017 <chandlerlei87@gmail.com>
2020-06-09 23:45:16 -0400

    add docker and postgresql

--- fakenews_backend/settings.py
+++ fakenews_backend/settings.py
@@ -72,11 +72,15 @@ WSGI_APPLICATION = 'fakenews_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+   
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'PASSWORD':'postgres'
     }
 }
 

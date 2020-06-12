dcf3730c2fc95dfacd16630016e3e2e8169c6b09
chandlerlei2017 <chandlerlei87@gmail.com>
2020-06-11 21:24:36 -0400

    graphene start

---
+++ fakenews_backend/schema.py
@@ -0,0 +1,10 @@
+import graphene
+
+class Query(graphene.ObjectType):
+    pass
+
+class Mutation(graphene.ObjectType):
+    pass
+
+# schema = graphene.Schema(query=Query, mutation=Mutation)
+schema = graphene.Schema()
--- fakenews_backend/settings.py
+++ fakenews_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Graphql
+
+GRAPHENE = {
+    'SCHEMA': 'fakenews_backend.schema.schema'
+}

642d1b59de2936859f0bbe37b2952067c80b9932
chandlerlei2017 <chandlerlei87@gmail.com>
2020-06-12 10:49:43 -0400

    rename to models

--- fakenews_backend/settings.py
+++ fakenews_backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'fakenews_backend.apps.users'
+    'fakenews_backend.models.users'
 ]
 
 MIDDLEWARE = [
